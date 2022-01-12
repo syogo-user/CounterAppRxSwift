0a9344eb3b1e5262183c29e2576aaca091628e0b
syogo-user <s.photo.trumpet.005@gmail.com>
2022-01-12 10:01:58 +0900

    CounterAppをRxSwiftに書き換えた

--- CounterAppRxSwift/ViewController.swift
+++ CounterAppRxSwift/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLable: UILabel!
+    @IBOutlet weak var countUpButton: UIButton!
+    @IBOutlet weak var countDownButton: UIButton!
+    @IBOutlet weak var countResetButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
+    private var viewModel: CounterRxViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+        setupViewModel()
     }
-
-
+    
+    private func setupViewModel() {
+        viewModel = CounterRxViewModel()
+        let input = CounterViewModelInput(
+            countUpButton: countUpButton.rx.tap.asObservable(), countDownButton: countDownButton.rx.tap.asObservable(), countResetButton: countResetButton.rx.tap.asObservable()
+        )
+        viewModel.setup(input: input)
+        
+        viewModel.outputs?.counterText
+            .drive(countLable.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
 }
 
